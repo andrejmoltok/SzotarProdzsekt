9e61d0c236fcc34c24b7bcc9ff87183748b6fac9
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-12-19 20:08:54 +0100

    testing stat.html canvas dimentions

--- stat.js
+++ stat.js
@@ -6,13 +6,42 @@ const ctx1 = bekuldo.getContext("2d");
 const magyarazo = document.getElementById('magyarazo');
 const ctx2 = magyarazo.getContext("2d");
 
-bekuldo.style.width = "550px";
-bekuldo.style.height = "350px";
-bekuldo.style.backgroundColor = "white";
+const canvas = [bekuldo,magyarazo];
+const context = [ctx1,ctx2];
 
-magyarazo.style.width = "550px";
-magyarazo.style.height = "350px";
-magyarazo.style.backgroundColor = "white";
+init(canvas,context);
 
-ctx1.translate(25,325);
-ctx2.translate(25,325);
+function init(con,ctx) {
+    for (let i = 0; i < con.length; i++) {
+        con[i].style.width = "600px";
+        con[i].style.height = "350px";
+        con[i].style.backgroundColor = "white";
+    }
+    for (let j = 0; j < ctx.length; j++) {
+        ctx[j].translate(10,120);
+        ctx[j].strokeStyle = "#a79b96";
+        ctx[j].lineWidth = 3.0;
+        ctx[j].fillStyle = "black";
+        ctx[j].font = '6px sans-serif';
+        ctx[j].fillText("(0,0)",3,10);
+        ctx[j].beginPath();
+        ctx[j].moveTo(0, 0);
+        ctx[j].lineTo(270, 0);
+        ctx[j].moveTo(20,20);
+        ctx[j].lineTo(20, -110);
+        ctx[j].stroke();
+        ctx[j].closePath();
+    }
+}
+
+function bekuldoCounter(user) {
+    let count = 0;
+    let arr = [];
+    for (let i = 0; i < szotar.length; i++) {
+
+    }
+}
+
+function magyarazoCounter() {
+
+}
