f452ecefe028942c38777b9865bba6d8655af234
Csokán Pál András <csokan.andras87@protonmail.ch>
2022-12-23 18:22:20 +0100

    created words list by users, minor updates

---
+++ words.js
@@ -0,0 +1,47 @@
+import { szotar } from './szotar.js';
+
+const user = document.getElementById('users');
+const bekuld = document.getElementById('bekuld');
+const magy = document.getElementById('magy');
+
+user.style.cursor = "pointer";
+bekuld.style.cursor = "pointer";
+magy.style.cursor = "pointer";
+
+let userek;
+
+users();
+
+function users() {
+    let userSet = new Set();
+    for (let i = 0; i < szotar.length; i++) {
+        if (szotar[i]["bekuldo2"] === undefined) {continue}
+        if (szotar[i]["magyarazo2"] === undefined) {continue}
+        userSet.add(szotar[i]["bekuldo2"]);
+        userSet.add(szotar[i]["magyarazo2"]);
+    }
+    console.log([...userSet]);
+    userek = [...userSet];
+    userek.forEach(function(v,i,a) {
+        user.innerHTML += `<div id="${v}"><a onclick="words('${v}')">${v}</a></div>`;
+    });
+}
+
+function words(username) {
+    const bekuld = document.getElementById('bekuld');
+    const magy = document.getElementById('magy');
+    
+    bekuld.innerHTML = '';
+    magy.innerHTML = '';
+    
+    for (let i = 0; i < szotar.length; i++) {
+        if (username === szotar[i]["bekuldo2"]) {
+            bekuld.innerHTML += `<div>${szotar[i]['halan']}</div>`;
+        }
+        if (username === szotar[i]["magyarazo2"]) {
+            magy.innerHTML += `<div>${szotar[i]['halan']}</div>`;
+        }
+    }
+}
+
+globalThis.words = words;
